7865a18a29310dc248be236b6eb278cd3a347e41
Madhuri-dev123 <gunnapananimadhuri@gmail.com>
2025-04-28 12:24:22 +0530

    calculater

---
+++ JAVASCRIPT/external.js
@@ -0,0 +1,21 @@
+   function abc(){
+       document.getElementById("d1").innerHTML="Hii"
+   }
+
+
+// 2.
+   let p1,p2,p3 ;
+   p1 = 100; p2 = 200; p3 = p1+p2;
+
+  document.getElementById("add").innerHTML= p3;
+
+  // 3.  let a,b,c;
+  a = 10 ;
+  b = 20 ;
+  c= a+b;
+  document.getElementById("add").innerHTML="The value of c is :" +c ;
+
+  
+
+
+
